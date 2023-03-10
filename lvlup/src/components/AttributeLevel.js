a632042456331bbce7f5a644192c47d17dd6b6b7
dex-devries <dexter.m.devries@gmail.com>
2023-03-10 10:07:21 -0700

    AttributeLevel and AttributeXP components

---
+++ lvlup/src/components/AttributeLevel.js
@@ -0,0 +1,9 @@
+const AttributeLevel = (props) => {
+  return (
+    <div className="attributeLevel">
+      Level: <div className='attributeData'>{99}</div>
+    </div>
+  );
+};
+
+export default AttributeLevel;
---
+++ lvlup/src/components/AttributeXP.js
@@ -0,0 +1,9 @@
+const AttributeXP = (props) => {
+  return (
+    <div className="attributeXP">
+      XP: <div className='attributeData'>{999999}</div>
+    </div>
+  );
+};
+
+export default AttributeXP;
