d0e54fea5e92a1423d03f3198fdd7816f49319d4
dex-devries <dexter.m.devries@gmail.com>
2023-03-10 08:38:26 -0700

    Header component with logo

---
+++ lvlup/src/components/Header.js
@@ -0,0 +1,11 @@
+import "./style.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={require("../assets/logo2.jpg")} alt="Level up logo"></img>
+    </div>
+  );
+};
+
+export default Header;
