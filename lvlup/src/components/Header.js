ad7b53e8408ae46fc9868f4197c30b4a0df98969
dex-devries <dexter.m.devries@gmail.com>
2023-03-10 09:26:50 -0700

    more styling attributes closer to header

--- lvlup/src/components/Header.js
+++ lvlup/src/components/Header.js
@@ -3,7 +3,7 @@ import "./styles.css";
 const Header = () => {
   return (
     <div className="header">
-      <img className="logoHeader" src={require("../assets/logo2.jpg")} alt="Level up logo" style={{width: '330px'}}></img>
+      <img className="logoHeader" src={require("../assets/logo2.jpg")} alt="Level up logo"></img>
     </div>
   );
 };
