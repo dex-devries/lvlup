3da48e5c7d014c8d6f802eaa9015023df7668e98
dex-devries <dexter.m.devries@gmail.com>
2023-03-13 07:15:15 -0700

    Add and Stats button images and styling

--- lvlup/src/components/Attribute.js
+++ lvlup/src/components/Attribute.js
@@ -3,29 +3,47 @@ import AttributeLevel from "./AttributeLevel";
 import AttributeXP from "./AttributeXP";
 
 const Attribute = (props) => {
-    const abilityType = props.type;
+  const abilityType = props.type;
 
-    // sort data for specific ability
+  // sort data for specific ability
 
-    // handler for add button clicked (opens add menu)
-    const addClickHandler = (e) => {
-        props.addMenuOpen(abilityType);
-    }
+  // handler for add button clicked (opens add menu)
+  const addClickHandler = (e) => {
+    props.addMenuOpen(abilityType);
+  };
 
-    return (
+  return (
     <div className={abilityType}>
-        <img className="abilityImage" src={require(`../assets/${abilityType}.png`)} alt={abilityType}/>
-        <h1 className="abilityHeader" style={{color: 'white'}}>{abilityType.toUpperCase()}</h1>
-        <div className="attributeStats">
-            <AttributeLevel/>
-            <AttributeXP/>
-        </div>
-        <div className="buttons">
-                <button className='addButton' onClick={addClickHandler}>Add</button>
-                <button className='viewButton'>View</button>
-        </div>
+      <img
+        className="abilityImage"
+        src={require(`../assets/${abilityType}.png`)}
+        alt={abilityType}
+      />
+      <h1 className="abilityHeader" style={{ color: "white" }}>
+        {abilityType.toUpperCase()}
+      </h1>
+      <div className="attributeStats">
+        <AttributeLevel />
+        <AttributeXP />
+      </div>
+      <div className="buttons">
+        <button title='Add activity' className="addButton" onClick={addClickHandler}>
+          <img
+            className="addThumb"
+            src={require("../assets/add-sm.png")}
+            alt="add button"
+          />
+        </button>
+        <button title='View stats' className="statsButton">
+          <img
+            className="statsThumb"
+            src={require("../assets/stats-neg-removebg.png")}
+            alt="stats button"
+          />
+        </button>
+      </div>
     </div>
-    )
-}
- 
+  );
+};
+
 export default Attribute;
