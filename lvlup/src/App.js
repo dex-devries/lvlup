cc259f82ceb4c12dadece04f571ab71ab17c676c
dex-devries <dexter.m.devries@gmail.com>
2023-03-14 10:34:41 -0700

    Added parseData function on demo data working

--- lvlup/src/App.js
+++ lvlup/src/App.js
@@ -1,11 +1,14 @@
 import Grid from "./components/Grid";
 import Header from "./components/Header";
 import {activities} from './assets/testData/demoData';
+import {parseData} from './dataHandler';
 
 const TEST_DATA = activities;
+const INIT_STATS_OBJ = parseData(TEST_DATA);
 
 function App() {
-  console.log(activities);
+  console.log("Stats object from parseData:");
+  console.log(INIT_STATS_OBJ);
   return (
     <>
       <Header />
