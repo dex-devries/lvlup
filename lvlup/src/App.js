76302f34379bf064ea50f252307dfca33c4b253f
dex-devries <dexter.m.devries@gmail.com>
2023-03-14 09:37:30 -0700

    created Demo data, added to App.js

--- lvlup/src/App.js
+++ lvlup/src/App.js
@@ -1,7 +1,11 @@
 import Grid from "./components/Grid";
 import Header from "./components/Header";
+import {activities} from './assets/testData/demoData';
+
+const TEST_DATA = activities;
 
 function App() {
+  console.log(activities);
   return (
     <>
       <Header />
