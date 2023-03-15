bfa506a7047304ff5e35da56304a66ece52f9a59
dex-devries <dexter.m.devries@gmail.com>
2023-03-15 12:55:55 -0700

    Badge and badgeHandler not working, crashing App require.context object can't find items

--- lvlup/src/components/Badge.js
+++ lvlup/src/components/Badge.js
@@ -1,36 +1,14 @@
-import { badges } from "../assets/icons/badgeMap";
+
+import { badgeHandler } from "./badgeHandler";
+
 import "./styles.css";
 
 const Badge = (props) => {
-  const level = props.level;
-  const attribute = props.attribute;
-  let collectedBadges = [["targeting.png", "TARGET ACQUIRED"]];
-
-  // logic to go through badgeMap and collect all badges
-
-  //   let i = 2;
-  //   while (i <= level) {
-  //     if (badges[attribute][i] !== undefined) {
-  //         collectedBadges.push(badges[attribute][i]);
-  //     }
-  //     i++;
-  //   }
-
-  // most recent (highest level) badge from the end of collectedB array
-  // [0] index at the end gets path, [1] would get badge name
-  const displayBadge =
-    "../assets/icons/" + collectedBadges[collectedBadges.length - 1][0];
-    
-    // const src=require(displayBadge);
-    // console.log(src);
+  const badgeSrc = badgeHandler(props.level, props.attribute);
 
   return (
     <div className="badge">
-      <img
-        className="badgeImg"
-        src={require("../assets/icons/targeting.png")}
-        alt="badge"
-      />
+      <img className="badgeImg" src={badgeSrc} alt="badge" />
     </div>
   );
 };
---
+++ lvlup/src/components/badgeHandler.js
@@ -0,0 +1,29 @@
+// import { badges } from "../assets/icons/badgeMap";
+import "./styles.css";
+
+const badgeHandler = (level, attribute) => {
+  const images = require.context("../assets/icons/badges", true);
+  let collectedBadges = [["./targeting.png", "TARGET ACQUIRED"]];
+
+  // logic to go through badgeMap and collect all badges
+
+  // let i = 2;
+  // while (i <= level) {
+  //   if (badges[attribute][i] !== undefined && badges[attribute][i].length === 2) {
+  //     //console.log(badges[attribute][i]);
+  //     collectedBadges.push(badges[attribute][i]);
+  //   }
+  //   i++;
+  // }
+
+  console.log(collectedBadges);
+  console.log(images);
+  // most recent (highest level) badge from the end of collectedB array
+  // [0] index at the end gets path, [1] would get badge name
+  let imgSrc = collectedBadges[collectedBadges.length - 1][0];
+  //imgSrc = collectedBadges[0][0];
+
+  return images(imgSrc);
+};
+
+export { badgeHandler };
