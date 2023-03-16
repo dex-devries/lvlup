b07b565741266afa83a995cd11fbacf2e419e515
dex-devries <dexter.m.devries@gmail.com>
2023-03-16 13:19:09 -0700

    Removed test console.logs

--- lvlup/src/App.js
+++ lvlup/src/App.js
@@ -7,8 +7,8 @@ const TEST_DATA = activities;
 const INIT_STATS_OBJ = parseData(TEST_DATA);
 
 function App() {
-  console.log("Stats object from parseData:");
-  console.log(INIT_STATS_OBJ);
+  // console.log("Stats object from parseData:");
+  // console.log(INIT_STATS_OBJ);
   return (
     <>
       <Header />
