f497fec3749c9937e78df4a07f79c4d1ded7084b
dex-devries <dexter.m.devries@gmail.com>
2023-03-21 10:04:25 -0700

    dynamicStats - state, passes handler func to Grid

--- lvlup/src/App.js
+++ lvlup/src/App.js
@@ -1,18 +1,48 @@
 import Grid from "./components/Grid";
 import Header from "./components/Header";
-import {activities} from './assets/testData/demoData';
-import {parseData} from './dataHandler';
+import { activities } from "./assets/testData/demoData";
+import { parseData } from "./dataHandler";
+import React, { useState } from "react";
 
 const TEST_DATA = activities;
-const INIT_STATS_OBJ = parseData(TEST_DATA);
 
 function App() {
-  // console.log("Stats object from parseData:");
-  // console.log(INIT_STATS_OBJ);
+  let dynamicStatsObj = parseData(TEST_DATA);
+  let activitiesList = TEST_DATA;
+  // console.log("dynamicStats object from parseData:");
+  // console.log(dynamicStatsObj);
+
+  // state for stats
+  const [stats, setStats] = useState(dynamicStatsObj);
+
+  // function to pass to Grid for data retreival
+  const addUserData = (newData) => {
+    // update activitiesList NOTE: could use parseData here but that would iter entire list
+    activitiesList.push(newData);
+    // only runs expensive parseData operation on the new data
+    let stats = parseData([newData]);
+    // console.log("Stats object from parseData:");
+    // console.log(stats);
+
+    // update dynamicStatsObject with new scores - only updates one attribute and power, saves iterating through all
+    dynamicStatsObj[newData.attribute]["xp"] += stats[newData.attribute]["xp"];
+    dynamicStatsObj[newData.attribute]["level"] += ( stats[newData.attribute]["level"] - 1 );
+    // update power
+    dynamicStatsObj["power"]["xp"] += stats["power"]["xp"];
+    dynamicStatsObj["power"]["level"] += ( stats["power"]["level"] - 1 );
+
+    console.log("Stats object after add:");
+    console.log(dynamicStatsObj);
+
+    // update state and activities list
+    // setUserData([...userData, newData]);
+    setStats(dynamicStatsObj);
+  };
+
   return (
     <>
       <Header />
-      <Grid stats={INIT_STATS_OBJ} />
+      <Grid stats={stats} dataHandler={addUserData}/>
     </>
   );
 }
