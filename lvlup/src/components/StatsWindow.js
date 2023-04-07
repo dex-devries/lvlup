1bed079c903317b194b8926c8856ccaff9b6773c
dex-devries <dexter.m.devries@gmail.com>
2023-04-07 06:18:42 -0700

    logic for data select for attr Activity component added with props

--- lvlup/src/components/StatsWindow.js
+++ lvlup/src/components/StatsWindow.js
@@ -1,4 +1,5 @@
 import "./StatsWindow.css";
+import Activity from "./Activity";
 
 // Expected props:
 // close() - closes window with null argument (close(null)) -> Grid.js
@@ -9,14 +10,33 @@ const StatsWindow = (props) => {
     props.close(null);
   };
 
-  console.log("Testing data prop passed from App/Grid");
-  console.log(props.data);
+  // TESTING
+  // console.log("Testing data prop passed from App/Grid");
+  // console.log(props.data);
+
+  // LOGIC FOR DATA SELECT
+  let selectData = [];
+  // select only activities with selected attribute (all activities for power)
+  if (props.attribute !== "power") {
+    for (let i=0; i<props.data.length; i++) {
+      if (props.data[i].attribute === props.attribute) {
+        selectData.push(props.data[i]);
+      }
+    }
+  }
+  // for power attr, all activities
+  else {
+    selectData = props.data;
+  }
 
   return (
+    
     <div className="">
+      <Activity attribute={props.attribute} data={selectData}/>
       <button className="menuButton" onClick={cancelHandler}>
         Cancel
       </button>
+
     </div>
   );
 };
