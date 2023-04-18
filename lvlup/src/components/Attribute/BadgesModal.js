119330fceeebac263290b0c3488bb0d312a7413f
dex-devries <dexter.m.devries@gmail.com>
2023-04-18 08:48:21 -0700

    init BadgesModal component

---
+++ lvlup/src/components/Attribute/BadgesModal.js
@@ -0,0 +1,9 @@
+import "./BadgesModal.css";
+
+const BadgesModal = () => {
+    return ( <div className="badgesModalContainer">
+        <h1 style={{"color": "white"}}>MODAL</h1>
+    </div> );
+}
+ 
+export default BadgesModal;
