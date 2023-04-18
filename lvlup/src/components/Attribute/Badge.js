389a4e5cef0ee5960fea15ff0c564d912fe8ea73
dex-devries <dexter.m.devries@gmail.com>
2023-04-18 08:48:02 -0700

    stateful badge

--- lvlup/src/components/Attribute/Badge.js
+++ lvlup/src/components/Attribute/Badge.js
@@ -1,13 +1,35 @@
-
 import "./Badge.css";
+import BadgesModal from "./BadgesModal";
+import ReactDOM from "react-dom";
+import React, { useState } from "react";
 
 // Expected props:
 // image object from require.context in badgeHandler.js
 const Badge = (props) => {
+  const [modalOpen, setModalOpen] = useState(false);
 
+  const hoverEnterHandler = () => {
+    setModalOpen(true);
+  };
+
+  const hoverExitHandler = () => {
+    // console.log("mouse exited badge");
+  };
+
+  if (modalOpen) {
+    const modalRoot = document.getElementById("modal-root");
+    ReactDOM.createPortal(<BadgesModal />, modalRoot);
+  }
+  
   return (
     <div className="badge">
-      <img className="badgeImg" src={props.image} alt="badge" />
+      <img
+        className="badgeImg"
+        src={props.image}
+        alt="badge"
+        onMouseEnter={hoverEnterHandler}
+        onMouseLeave={hoverExitHandler}
+      />
     </div>
   );
 };
