cc719f5023bb1c32f414062f4dd6d004405fa865
dex-devries <dexter.m.devries@gmail.com>
2023-04-20 14:04:53 -0700

    Badge window init - branch from here Changing direction from modal to BadgeWindow

--- lvlup/src/components/Attribute/Badge.js
+++ lvlup/src/components/Attribute/Badge.js
@@ -5,8 +5,12 @@ import "./Badge.css";
 // image object from require.context in badgeHandler.js
 const Badge = (props) => {
 
+  const badgeClickHandler = () => {
+    console.log("clicked badge");
+  }
+
   return (
-    <div className="badge">
+    <div className="badge" onClick={badgeClickHandler}>
       <img className="badgeImg" src={props.image} alt="badge" />
     </div>
   );
