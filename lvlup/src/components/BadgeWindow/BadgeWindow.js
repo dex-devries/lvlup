b71ddb48b84874b4c8a845e2ba0d6eaedf0a6d7f
dex-devries <dexter.m.devries@gmail.com>
2023-04-21 07:45:41 -0700

    badgeWindow state in Grid.js

---
+++ lvlup/src/components/BadgeWindow/BadgeWindow.js
@@ -0,0 +1,5 @@
+const BadgeWindow = () => {
+    return ( <></> );
+}
+ 
+export default BadgeWindow;
--- lvlup/src/components/Grid.js
+++ lvlup/src/components/Grid.js
@@ -3,6 +3,7 @@ import Attribute from "./Attribute/Attribute";
 import ActivityMenu from "./ActivityMenu/ActivityMenu";
 import StatsWindow from "./StatsWindow/StatsWindow";
 import React, { useState } from "react";
+import BadgeWindow from './BadgeWindow/BadgeWindow';
 
 // Suggested modifications:
 // ActivityMenu opens as modal
@@ -22,6 +23,8 @@ const Grid = (props) => {
   const [menuOpenAttribute, setMenuOpenAttribute] = useState(null);
   // state for statsWindow - same logic as menuOpenAttr... name of attr loads correct stats
   const [statsWindowAttribute, setStatsWindowAttribute] = useState(null);
+  // state for badgeWindow - uses bool
+  const [badgeWindowOpen, setBadgeWindowOpen] = useState(false);
 
   // function for add button to pass to Attributes (opens acitivty menu)
   const addButtonClicked = (attribute) => {
@@ -31,6 +34,10 @@ const Grid = (props) => {
   const statsButtonClicked = (attribute) => {
     setStatsWindowAttribute(attribute);
   };
+  // function for badge to pass to Attributes (opens badge window)
+  const badgeClicked = (bool) => {
+    setBadgeWindowOpen(bool);
+  }
 
   // add menu open - menuOpenAttr != null , menuOpenAttr == attribute
   if (menuOpenAttribute) {
@@ -50,6 +57,12 @@ const Grid = (props) => {
     return menu;
   }
 
+  // badgeWindow open
+  if (badgeWindowOpen) {
+    menu = <BadgeWindow close={badgeClicked} data={props.data}/>
+    return menu;
+  }
+
   return (
     <>
       {/* {menu} */}
@@ -58,48 +71,56 @@ const Grid = (props) => {
           className="agility"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.agility}
         />
         <Attribute
           className="intellect"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.intellect}
         />
         <Attribute
           className="strength"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.strength}
         />
         <Attribute
           className="skill"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.skill}
         />
         <Attribute
           className="attack"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.attack}
         />
         <Attribute
           className="presence"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.presence}
         />
         <Attribute
           className="alchemy"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.alchemy}
         />
         <Attribute
           className="power"
           addMenuOpen={addButtonClicked}
           statsWindowOpen={statsButtonClicked}
+          badgeWindowOpen={badgeClicked}
           stats={props.stats.power}
         />
       </div>
