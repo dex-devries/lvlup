49237aa1a7589d964a9fe70074838223e03a9173
dex-devries <dexter.m.devries@gmail.com>
2023-04-21 07:45:10 -0700

    BadgeWindow instead of BadgesModal

--- lvlup/src/components/Attribute/BadgesModal.js
+++
@@ -1,9 +0,0 @@
-import "./BadgesModal.css";
-
-const BadgesModal = () => {
-    return ( <div className="badgesModalContainer">
-        <h1>MODAL</h1>
-    </div> );
-}
- 
-export default BadgesModal;
