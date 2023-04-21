ef6d0ee5f669fcd0ec6a13d5ff1e8cafc8240282
dex-devries <dexter.m.devries@gmail.com>
2023-04-21 09:06:17 -0700

    BadgeWindow working with headers/badges/styles

--- lvlup/src/components/BadgeWindow/BadgeWindow.js
+++ lvlup/src/components/BadgeWindow/BadgeWindow.js
@@ -1,5 +1,28 @@
-const BadgeWindow = () => {
-    return ( <></> );
-}
- 
+import { badgeCollector } from "../Logic/badgeCollectionHandler";
+import "./BadgeWindow.css";
+
+const BadgeWindow = (props) => {
+  console.log(props.stats);
+  const badges = badgeCollector(props.stats);
+  const images = require.context("../../assets/icons/badges", true);
+
+  let badgesJSX = [];
+  for (let attr in badges) {
+    badgesJSX.push(<h2 key={attr} className="badgeAttrHead">{attr.toUpperCase()}</h2>);
+
+    let attrBadges = [];
+    for (let badge of badges[attr]) {
+        // logic to exclude targeting.png - by default included from badgeHandler
+        if (badge[0] !== './targeting.png') {
+            // push badge img JSX
+            attrBadges.push(<img className="badgeImg" src={images(badge[0])} key={badge[0]} alt="badge"/>);
+        }      
+    }
+
+    badgesJSX.push(<div className="attrBadgesContainer" key={attr+"BadgesContainer"}>{attrBadges}</div>)
+  }
+
+  return <div className="badgeWindowCard">{badgesJSX}</div>;
+};
+
 export default BadgeWindow;
--- lvlup/src/components/Logic/badgeCollectionHandler.js
+++ lvlup/src/components/Logic/badgeCollectionHandler.js
@@ -1,11 +1,11 @@
 import { badgeHandler } from "./badgeHandler";
-import { attributes } from "./attributesList";
 
 const badgeCollector = (stats) => {
   let badgesObject = {};
-  for (let attr of stats) {
+  for (let attr in stats) {
     badgesObject[attr] = badgeHandler(stats[attr].level, attr);
   }
+  return badgesObject;
 };
 
 export {badgeCollector};
